73f095d25605cb11005388ee0cb96858cdc6844e
xuefzhao <xuefzhao@umich.edu>
2016-05-14 01:53:06 -0400

    setup.py updated 20160514

--- setup.py
+++ setup.py
@@ -1,35 +1,30 @@
-try:
-    from setuptools import setup
-except ImportError:
-    	from distutils.core import setup
-
+from setuptools import setup
+from distutils.core import setup,Extension
 from Cython.Build import cythonize
-
-def read(filename):
-   with open(os.path.join(os.path.dirname(__file__), filename)) as f:
-       return f.read()
-
 import os
 import sys
-import setuptools
-setuptools.setup(
+setup(
    name="VaLoR",
    version="0.0.1",
    author="Xuefang Zhao, University of Michigan",
    author_email="xuefzhao@umich.edu",
-   description="Randomized tool to detect and resolve complex structural variants.",
-   long_description=read("README.md"),
-   packages=["valor"],
+   description="Long read based genomic structural variants validator.",
+   packages=["valor_vali"],
+   scripts=["valor_vali/valor", "valor_vali/VaLoR"],
    package_data={
-       "valor": [
+       "valor_vali": [
            "templates/pred_config",
            "templates/truth_config",
            "contrib/SMCScoring.py",
        ],
    },
-   license="Propriety",
-   keywords="Complex Structural Variants",
-   classifiers=[
+    ext_modules = cythonize("valor_vali/*.pyx"),
+     install_requires=[
+     'cython', 'numpy','scipy','matplotlib','sklearn'
+      ],
+    license="Propriety",
+    keywords="Long read",
+    classifiers=[
        "Development Status :: 1 - Planning",
        "Environment :: Console",
        "Intended Audience :: Information Technology",
@@ -40,16 +35,5 @@ setuptools.setup(
        "Programming Language :: Python :: 2 :: Only",
        "Topic :: Scientific/Engineering :: Bio-Informatics",
    ],
-   entry_points={
-       "console_scripts": [
-           "VaLoR=valor.__main__:main",
-           "valor=valor.__main__:main",
-       ]
-   },
 )
 
-
-
-setup(
-    ext_modules = cythonize("VaLoR/*.pyx")
-)
